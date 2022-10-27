fa87cdedcc257647af898ec9acf99e97a8a1562c
Hernan Maslowski <hernan@maslowski.com.ar>
2022-10-27 19:27:12 +0200

    Just another commit

--- commit.ps1
+++ commit.ps1
@@ -1,4 +1,5 @@
-$commit_msg = read-host "Commit message"
+$def_msg = "Just another commit"
+$commit_msg = if($commit_msg = (Read-Host "Commit message -Default: $def_msg]")){$commit_msg}else{$def_msg}
 git add .
 git commit -am "$commit_msg"
 git push
